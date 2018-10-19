fd66cf635ff45a790c2b27137d7b5a1b2798543a
SydvestHaavard <33754259+SydvestHaavard@users.noreply.github.com>
2018-10-19 15:39:28 +0200

    Update sydvest-admin-panel.php

--- sydvest-admin-panel.php
+++ sydvest-admin-panel.php
@@ -1,6 +1,6 @@
 <?php 
 /**
-* Plugin Name: Sydvest Admin Panel
+* Plugin Name: Sydvest Adminpanel
 * Plugin URI: http://sydvest.no/
 * Description: Sydvest-tema for administrasjonsområdet.
 * Based on Oktan Admin 1.0 by Øyvind Eikeland <oyvind.eikeland@oktan.no>. Edited 19.10.2018 by Håvard Hvoslef Kvalnes <haavard@sydvest.no>.
