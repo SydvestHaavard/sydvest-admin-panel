a3639244a9a9c79b663f9e96793a6a052edcf8a4
SydvestHaavard <33754259+SydvestHaavard@users.noreply.github.com>
2018-10-19 15:44:13 +0200

    Update sydvest-admin-panel.php

--- sydvest-admin-panel.php
+++ sydvest-admin-panel.php
@@ -29,7 +29,7 @@ function register_sv_styles() {
  
 // Change footer text
 function sv_change_footer_text() {
-	echo 'SYDVEST - Kundeservice Kontakt '. SVNAME .' <a href="mailto:'. SVEMAIL .'" title="Send e-post til support">'. SVEMAIL .'</a> - Telefon '. SVPHONE .' <a href="http://sydvest.no" target="_blank"><img src="/wp-content/plugins/sydvest-admin-panel/img/SYDVEST_logo_farge.svg" alt="SYDVEST Logo" /></a>';
+	echo '<a href="http://sydvest.no" target="_blank"><img src="/wp-content/plugins/sydvest-admin-panel/img/SYDVEST_logo_farge.svg" alt="SYDVEST Logo" /></a> &ndash; Kundeservice Kontakt '. SVNAME .' <a href="mailto:'. SVEMAIL .'" title="Send e-post til support">'. SVEMAIL .'</a> &ndash; Telefon '. SVPHONE .'';
 }
 
 // DASHBOARD REMOVE WIDGETS
