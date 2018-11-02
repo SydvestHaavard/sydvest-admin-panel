87426d2cade204509c365ec38022322ffe42f6d2
SydvestHaavard <33754259+SydvestHaavard@users.noreply.github.com>
2018-11-02 13:29:06 +0100

    Update sydvest-admin-panel.php

--- sydvest-admin-panel.php
+++ sydvest-admin-panel.php
@@ -4,10 +4,12 @@
 * Plugin URI: http://sydvest.no/
 * Description: Sydvest-tema for administrasjonspanelet.
 * Based on Oktan Admin 1.0 by Øyvind Eikeland <oyvind.eikeland@oktan.no>. Edited 19.10.2018 by Håvard Hvoslef Kvalnes <haavard@sydvest.no>.
-* Version: 1.1.11
+* Version: 1.1.12
 * Author: Sydvest AS <post@sydvest.no> 
 * Author URI: http://sydvest.no/
 * Icon: https://raw.github.com/SydvestHaavard/sydvest-admin-panel/master/img/icon.svg
+* Icon 1x: https://raw.github.com/SydvestHaavard/sydvest-admin-panel/master/img/icon-128x128.png
+* Icon 2x: https://raw.github.com/SydvestHaavard/sydvest-admin-panel/master/img/icon-256x256.png
 * BannerHigh: https://raw.github.com/SydvestHaavard/sydvest-admin-panel/master/img/banner-1544x500.png
 * BannerLow: https://raw.github.com/SydvestHaavard/sydvest-admin-panel/master/img/banner-722x250.png
  */
